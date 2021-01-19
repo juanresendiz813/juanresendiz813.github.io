184640818d8018d0718f444e972a52278e5b4df5
JRZ813 <50384386+juanresendiz813@users.noreply.github.com>
2021-01-19 00:23:34 -0500

    Made new header and added a picture

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  var x = document.getElementById("headerNav");
+  if (x.className === "headerNav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "headerNav";
   }
 }
